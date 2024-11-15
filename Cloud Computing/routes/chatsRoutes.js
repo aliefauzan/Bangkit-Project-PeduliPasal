f49771cd2867da3b630a8bb87ce8977b11339465
Andi Muhammad Alief Fauzan <145396717+aliefauzan@users.noreply.github.com>
2024-11-15 17:10:32 +0800

    add post addMessageToChat, get getChatMessageById

--- Cloud Computing/routes/chatsRoutes.js
+++ Cloud Computing/routes/chatsRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const asycnHandler = require('express-async-handler');
-const { createChat } = require('../controllers/chatController')
+const { 
+    createChat,
+    addMessageToChat,
+    getChatMessageById,
+ } = require('../controllers/chatController');
 
-router.post('/',asycnHandler(createChat))
+router.post('/',asycnHandler(createChat));
+router.post('/:chatId', asycnHandler(addMessageToChat)); 
+router.get('/:chatId', asycnHandler(getChatMessageById)); 
 
 module.exports = router
