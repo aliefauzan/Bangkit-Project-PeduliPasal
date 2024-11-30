f7e902c0cd030ba5e14d86d745f2895e299acfd7
Satriya Wibowo <c14220311@john.petra.ac.id>
2024-11-30 12:52:07 +0700

    adding gemini ai to answer quiz

--- Mobile Development/app/src/main/java/com/example/pedulipasal/adapter/gamification/QuestionAdapter.kt
+++ Mobile Development/app/src/main/java/com/example/pedulipasal/adapter/gamification/QuestionAdapter.kt
@@ -5,18 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pedulipasal.data.model.gamification.QuestionsItem
 import com.example.pedulipasal.databinding.ItemQuestionBinding
+import com.example.pedulipasal.helper.Result
+import com.google.ai.client.generativeai.type.GenerateContentResponse
 
 class QuestionAdapter(
     private val context: Context,
-    private val questionList: List<QuestionsItem?>?, // Made non-nullable
     private val onItemSelectedCallback: OnItemSelected
 ) : RecyclerView.Adapter<QuestionAdapter.ViewHolder>() {
 
+    private val questionList: MutableList<QuestionsItem?> = mutableListOf()
+
     interface OnItemSelected {
-        fun onOptionClicked(chosenOption: String, correctAnswer: String)
+        fun onOptionClicked(chosenOption: String, correctAnswer: String, currPostion: Int)
+        fun onAskGeminiClicked(prompt: String): LiveData<Result<GenerateContentResponse>>
     }
 
     class ViewHolder(val binding: ItemQuestionBinding): RecyclerView.ViewHolder(binding.root)
@@ -31,9 +37,11 @@ class QuestionAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val question = questionList?.get(position) ?: QuestionsItem()
+        val question = questionList[position] ?: QuestionsItem()
+        var prompt = ""
         holder.binding.apply {
             tvQuestionItem.text = question.question ?: "No Question"
+            prompt += "${question.question} \n"
             rgAnswerOptions.removeAllViews()
 
             question.options?.forEachIndexed { _, option ->
@@ -44,16 +52,58 @@ class QuestionAdapter(
                 radioButton.setOnClickListener {
                     onItemSelectedCallback.onOptionClicked(
                         chosenOption = radioButton.text.toString(),
-                        correctAnswer = question.correctAnswer ?: "No Answer"
+                        correctAnswer = question.correctAnswer ?: "No Answer",
+                        currPostion = position
                     )
+
+                    if (radioButton.text.toString() == question.correctAnswer) {
+                        tvGeminiAnswer.visibility = View.GONE
+                        for (i in 0 until rgAnswerOptions.childCount) {
+                            val child = rgAnswerOptions.getChildAt(i)
+                            if (child is RadioButton) {
+                                child.isEnabled = false
+                            }
+                        }
+                    }
                 }
-                // Add radio button to RadioGroup
+                prompt += "${radioButton.text} \n"
                 rgAnswerOptions.addView(radioButton)
+            }
+
+            btnAskGemini.setOnClickListener {
+                btnAskGemini.visibility = View.GONE
+                val response = onItemSelectedCallback.onAskGeminiClicked(prompt)
+                response.observe(context as LifecycleOwner) { result ->
+                    if (result is Result.Success) {
+                        progressBar.visibility = View.GONE
+                        tvGeminiAnswer.text = result.data.text
+                        tvGeminiAnswer.visibility = View.VISIBLE
+                    }
+                    if (result is Result.Loading) {
+                        progressBar.visibility = View.VISIBLE
+                    }
+                    if (result is Result.Error) {
+                        progressBar.visibility = View.GONE
+                        tvGeminiAnswer.text = result.error
+                        tvGeminiAnswer.visibility = View.VISIBLE
+                    }
+                }
             }
         }
     }
 
+    fun setQuestions(questions: List<QuestionsItem?>) {
+        questionList.clear()
+        questionList.addAll(questions)
+        notifyDataSetChanged()
+    }
+
+    fun clearAllItems() {
+        questionList.clear()
+        notifyDataSetChanged()
+    }
+
     override fun getItemCount(): Int {
-        return questionList?.size ?: -1
+        return questionList.size
     }
 }
